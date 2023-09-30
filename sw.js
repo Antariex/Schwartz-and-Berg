a98bf414d857d93c3bfeb36b9a182e194f93794b
Ariel Antequiera <arielantequiera@gmail.com>
2023-09-30 15:29:14 -0300

    Se agrega Service worker

--- sw.js
+++ sw.js
@@ -1,25 +1,31 @@
 self.addEventListener("install", async (event) => {
-    // Precache
-    const cache = await caches.open("recursos");
-    await cache.addAll(["/", "styles.css", "/images/logo.jpg", "https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap", "https://fonts.gstatic.com/s/roboto/v30/KFOlCnqEu92Fr1MmSU5fBBc4.woff2"]);
-  });
-  
-  self.addEventListener("activate", (event) => {
-    // Se activó el SW para la PWA de este scope
-  });
-  
-  self.addEventListener("fetch", (event) => {
-    event.respondWith(handleRequest(event.request));
-  });
+  // Precache
+  const cache = await caches.open("recursos");
+  await cache.addAll([
+    "/",
+    "styles.css",
+    "/images/logo.jpg",
+    "https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap",
+    "https://fonts.gstatic.com/s/roboto/v30/KFOlCnqEu92Fr1MmSU5fBBc4.woff2",
+  ]);
+});
 
-  async function handleRequest(request) {
-    const cache = await caches.open("recursos");
+self.addEventListener("activate", (event) => {
+  // Se activó el SW para la PWA de este scope
+});
 
-    const cachedResponse = await cache.match(request);
-    if  (cachedResponse) return cachedResponse;
+self.addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
+});
 
-    const networkResponse = await fetch(request);
-    //cache.put(request, networkResponse.clone());
-    
-    return networkResponse; // Change this line from 'fetch(response)' to 'fetch(event.request)'
-        };  
+async function handleRequest(request) {
+  const cache = await caches.open("recursos");
+
+  const cachedResponse = await cache.match(request);
+  if (cachedResponse) return cachedResponse;
+
+  const networkResponse = await fetch(request);
+  //cache.put(request, networkResponse.clone());
+
+  return networkResponse; // Change this line from 'fetch(response)' to 'fetch(event.request)'
+}
