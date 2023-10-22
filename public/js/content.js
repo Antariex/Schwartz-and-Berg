bf6fd7149ff9672d65e5b8db75a5ed1c0008e964
Ariel Antequiera <arielantequiera@gmail.com>
2023-10-22 02:59:29 -0300

    Actualizar content.js

--- public/js/content.js
+++ public/js/content.js
@@ -1,24 +1,24 @@
 // Hero image changer
             // Arreglo de rutas de las imágenes que deseas mostrar
             const imagenes = [
-                "../images/hotpastrami.jpg",
-                "../images/vareniques.jpg",
-                "../images/ribs.jpg",
-                "../images/burguer.jpg",
-                "../images/goulash.jpg",
-                "../images/chessecake.jpg"
+                "./images/hotpastrami.jpg",
+                "./images/vareniques.jpg",
+                "./images/ribs.jpg",
+                "./images/burguer.jpg",
+                "./images/goulash.jpg",
+                "./images/chessecake.jpg"
             ];
 
             const imagenElement = document.getElementById("imagenReemplazar");
             let currentIndex = 0;
 
-            // Función para cambiar la imagen cada 3 segundos
+            // Función para cambiar la imagen cada 2 segundos
             function cambiarImagen() {
                 imagenElement.src = imagenes[currentIndex];
                 currentIndex = (currentIndex + 1) % imagenes.length;
             }
 
-            // Establecer un intervalo para llamar a la función cada 3 segundos
+            // Establecer un intervalo para llamar a la función cada 2 segundos
             const intervalo = setInterval(cambiarImagen, 2000);
 
             // Detener el intervalo cuando sea necesario (por ejemplo, al hacer clic en la imagen)
